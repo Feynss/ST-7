b5e93b4a680209a0fd935b0e129b241292f5ebaa
Feynss <michavasnn@gmail.com>
2026-06-04 22:04:15 +0300

    first part

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,39 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class App {
+
+    static WebDriver browser;
+
+    public static void main(String[] args) {
+        initDriver();
+        fetchPassword();
+        IpChecker.execute(browser);
+        WeatherFetcher.execute(browser);
+        browser.quit();
+    }
+
+    static void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "/home/feynss/chromedriver/chromedriver");
+        browser = new ChromeDriver();
+    }
+
+    static void fetchPassword() {
+        try {
+            browser.get("https://www.calculator.net/password-generator.html");
+            WebDriverWait w = new WebDriverWait(browser, Duration.ofSeconds(15));
+            String pwd = w.until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.verybigtext b"))
+            ).getText();
+            System.out.println("Результат: " + pwd);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
